899272e4171a70aed844431bc8b01f4c6504c0b1
JosueSachun <josuesachung@gmail.com>
2024-01-19 11:32:34 -0500

    Nombre del proyecto: cursoJava, Descripción: Udemy, versión prueba

---
+++ src/main/java/com/josue/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.josue.myapp.entity;
+
+/**
+ * @author: Josue
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilación del archivo TestEntity.
+	 */
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
